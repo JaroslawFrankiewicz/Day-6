edb75b509b2bfb3ee2518dd0582bfcaadeb800fb
Jarek Frankiewicz <frantz12@wp.pl>
2024-02-18 18:30:31 +0100

    Day 6

---
+++ Day 6/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    internal class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string firstName, string sureName, int age)
+        {
+            this.FirstName = firstName;
+            this.SureName = sureName;
+            this.Age = age;
+        }
+        public string FirstName
+        {
+            get;
+            private set;
+        }
+        public string SureName
+        {
+            get;
+            private set;
+        }
+        public int Age
+        {
+            get;
+            private set;
+        }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+    }
+}
--- Day 6/Program.cs
+++ Day 6/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using Day_6;
+
+Employee employee1 = new Employee("Adam", "Nowak", 24);
+Employee employee2 = new Employee("Monika", "Kowalska", 25);
+Employee employee3 = new Employee("Zuzia", "Stępień", 27);
+
+employee1.AddScore(5);
+employee1.AddScore(6);
+employee1.AddScore(7);
+employee1.AddScore(2);
+employee1.AddScore(3);
+
+employee2.AddScore(9);
+employee2.AddScore(4);
+employee2.AddScore(8);
+employee2.AddScore(2);
+employee2.AddScore(3);
+
+employee3.AddScore(7);
+employee3.AddScore(6);
+employee3.AddScore(2);
+employee3.AddScore(9);
+employee3.AddScore(1);
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
+    }
+}
+Console.WriteLine("Pracownik: " + employeeWithMaxResult.FirstName + " " + employeeWithMaxResult.SureName + " lat " + employeeWithMaxResult.Age + " uzyskał(-a) najwięcej punktów: " + maxResult);
